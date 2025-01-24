23d8d9f35cbf6a4a56b10c6f1dd6732f98f965e9
Prajwal-H-Rao <prajwalhr130@gmail.com>
2025-01-24 07:46:08 +0530

    didn't commit server

--- server/server.js
+++ server/server.js
@@ -1,15 +1,27 @@
 const dotenv = require("dotenv");
 const cors = require("cors");
 const express = require("express");
+const mongoose = require("mongoose");
 
 dotenv.config();
 
 const app = express();
+const port = process.env.PORT || 4000;
+
+const authRoutes = require("./routes/auth");
+const taskRoutes = require("./routes/tasks");
 
 app.use(cors());
 app.use(express.json());
+app.use("/auth", authRoutes);
+app.use("/tasks", taskRoutes);
 
-const port = process.env.PORT || 4000;
-app.listen(port, () => {
-  console.log(`Listening on port:${port}`);
-});
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("Connected to mongoDB");
+    app.listen(port, () => {
+      console.log(`Server running on port:${port}`);
+    });
+  })
+  .catch((error) => console.error("MongoDB connection error:", error));
